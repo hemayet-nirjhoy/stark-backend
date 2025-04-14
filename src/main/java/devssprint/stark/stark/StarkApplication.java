25c2186f9de0e05ec7ca4f47ff53e1c5b583e3b6
Mohammad Hemayet Ullah <40654463+hemayet-nirjhoy@users.noreply.github.com>
2025-04-15 01:00:47 +0200

    initial project setup

---
+++ src/main/java/devssprint/stark/stark/StarkApplication.java
@@ -0,0 +1,13 @@
+package devssprint.stark.stark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/devssprint/stark/stark/StarkApplicationTests.java
@@ -0,0 +1,13 @@
+package devssprint.stark.stark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
