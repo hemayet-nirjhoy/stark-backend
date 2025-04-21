d69de172f197ab737bd7cccb4d65993b9440fb99
Mohammad Hemayet Ullah <40654463+hemayet-nirjhoy@users.noreply.github.com>
2025-04-22 00:58:58 +0200

    integrated new library for json web token

--- src/main/java/devssprint/stark/stark/model/UserProfile.java
+++ src/main/java/devssprint/stark/stark/model/UserProfile.java
@@ -1,14 +1,8 @@
 package devssprint.stark.stark.model;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
 
 @Entity
-@Getter
-@Setter
-@ToString
 @Table (name = "user_profile")
 public class UserProfile {
     @Id
