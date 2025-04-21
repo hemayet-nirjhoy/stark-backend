dbc5853452662333f242ff94f94e6f912a4697d9
Mohammad Hemayet Ullah <40654463+hemayet-nirjhoy@users.noreply.github.com>
2025-04-22 01:00:45 +0200

    JWT Token configurations

---
+++ src/main/java/devssprint/stark/stark/secutiry/SecurityConfig.java
@@ -0,0 +1,45 @@
+package devssprint.stark.stark.secutiry;
+
+import org.hibernate.annotations.ConcreteProxy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig{
+
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
+    @Autowired
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .exceptionHandling(exceptionHandling -> exceptionHandling.authenticationEntryPoint(jwtAuthenticationEntryPoint))
+                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers("/account/signing").permitAll()
+                        .requestMatchers("/account/forget-password").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/imgs/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/documents/**").permitAll()
+                        .requestMatchers("/").permitAll()
+                        .requestMatchers("/admin/**").hasAuthority("ADMIN")
+                        .anyRequest().permitAll());
+
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+
+}
